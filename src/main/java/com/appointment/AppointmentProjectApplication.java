41793cdf22210150ccaa0bd303616bc879291b66
RendevMq <rensystem73@gmail.com>
2024-12-04 23:58:04 -0500

    first commit

---
+++ src/main/java/com/appointment/AppointmentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentProjectApplication.class, args);
+	}
+
+}
